696a39574656bf1cf98d0d102161bc352646552d
Sergey Pozhilov <pozhilov@gmail.com>
2018-11-29 16:26:58 +0300

    Inner page template - copy of home

--- index.php
+++ index.php
@@ -13,12 +13,19 @@
  */
 
 get_header(); ?>
-
-<?php
-if ( have_posts() ) : while ( have_posts() ) : the_post();
-
-endwhile; endif;
-?>
-
+	<header class="hero" id="scene" data-selector=".scene-child">
+		<div class="hero__bg"><div data-depth="0.3" class="scene-child"> </div></div>
+		<div class="hero__wrapper" id="hero-content">
+			<h1 class="hero__caption" id="js-greeting"
+			    style="filter: blur(0px);">WordPress<br>Solutions</h1>
+			<h2 class="hero__intro" id="hero-intro"
+			    style="filter: blur(0px); transform: translate(0px, 0px);">
+				Websites, Online Stores, Membership Sites, Custom Plugins. Nicely designed and built using WordPress.
+			</h2>
+		</div>
+	</header>
+	<article class="main">
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); the_content(); endwhile; endif; ?>
+	</article>
 <?php
 get_footer();
